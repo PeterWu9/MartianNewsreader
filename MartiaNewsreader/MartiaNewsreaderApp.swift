bfd21306485da788a7f0771f280e7bc8377954fa
Peter Wu <petey888@gmail.com>
2022-06-25 13:59:35 -0700

    Initial Commit

---
+++ MartiaNewsreader/MartiaNewsreaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  MartiaNewsreaderApp.swift
+//  MartiaNewsreader
+//
+//  Created by Peter Wu on 6/25/22.
+//
+
+import SwiftUI
+
+@main
+struct MartiaNewsreaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
